12400ff58366780c972874f3912e2bdcb453aeb5
Rawan <rawnq.sb@gmail.com>
2023-02-28 22:07:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {document.querySelector("p").textContent= "This is really cool!"
+console.log("I work")})
+
+
+//document.addEventListener("DOMContentLoaded", function() {
+//  console.log("The DOM has loaded");
+//});
+
+
+  
